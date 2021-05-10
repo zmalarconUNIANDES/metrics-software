43bfa148e4326a947d3592d14b9b9af412759e58
Jimmy <jmmy.cardenas@avaldigitallabs.com>
2021-05-10 00:23:55 -0500

    Update coverage

--- src/app/commons/pipes/image-url.pipe.spec.ts
+++ src/app/commons/pipes/image-url.pipe.spec.ts
@@ -1,8 +1,31 @@
 import { ImageUrlPipe } from './image-url.pipe';
+import { async, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { environment as ENV } from '@environment';
 
 describe('ImageUrlPipe', () => {
+  let pipe: ImageUrlPipe;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    pipe = new ImageUrlPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new ImageUrlPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should be null', () => {
+    expect(pipe.transform(null)).toBeNull();
+  });
+
+  it('should be correct', () => {
+    const name = 'image';
+    expect(pipe.transform(name)).toEqual(ENV.resources.base_img + name);
   });
 });
